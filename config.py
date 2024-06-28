0b0afda5dc48856140168264cca8aeb6f170e949
TickTockBent <benttick@gmail.com>
2024-06-28 15:46:27 -0400

    correcting config

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 BOT_TOKEN = 'MTI1NTI3MjAyMDQ3NjgyMTUzNQ.GkVw5g.7xI6KrQtdkfhP2xAdCVcU75CdrGc_4ML-WACXU'
 ADMIN_IDS = [170855291636809728]  # List of admin user IDs
 DB_FILE = 'user_data.db'
-MIN_TIP_AMOUNT = 1  # Minimum tip amount
+MIN_TIP_AMOUNT = 0.1  # Minimum tip amount
 MAX_TIP_AMOUNT = 1000  # Maximum tip amount
 BOT_USER_ID = '1255272020476821535'  # Replace with your bot's user ID
 BOT_WALLET = 'sm1qqqqqq8qpyc9eyu35dy09qlehf5snt276pl6cach4crhj'  # Replace with your bot's wallet address
