8d382b35e1aa735ee8bc5e1b324f755d63e29be3
TickTockBent <benttick@gmail.com>
2024-06-29 09:04:53 -0400

    remove gen

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,8 @@
 from .database import init_db, get_user_data, update_user_balance, log_action
-from .address_validator import generate_spacemesh_address, validate_spacemesh_address
+from .address_validator import validate_spacemesh_address
+from .spacemesh_wallet import spawn_wallet_address  # Add this line
 
 __all__ = [
     'init_db', 'get_user_data', 'update_user_balance', 'log_action',
-    'generate_spacemesh_address', 'validate_spacemesh_address'
+    'spawn_wallet_address', 'validate_spacemesh_address'
 ]
